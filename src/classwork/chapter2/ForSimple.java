f434226e47dc6fa03c3d314fb66da9aa7051cbb0
Mari-Adamyan <adamyanmari3@gmail.com>
2023-08-24 21:42:38 +0400

    homework 1

---
+++ src/classwork/chapter2/ForSimple.java
@@ -0,0 +1,10 @@
+package classwork.chapter2;
+
+public class ForSimple {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 10; x++) {
+            System.out.println("This is x:" + x);
+        }
+    }
+}
